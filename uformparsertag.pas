1e4cdf42e06aef3a34a4d8b738953ea267592d3f
rasberryrabbit <lovelust@hanmail.net>
2018-04-29 14:03:03 +0900

    add missing files.

---
+++ uformparsertag.pas
@@ -0,0 +1,41 @@
+unit uformParserTag;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ValEdit, StdCtrls;
+
+type
+
+  { TFormParserSet }
+
+  TFormParserSet = class(TForm)
+    ButtonOk: TButton;
+    ButtonCancel: TButton;
+    ValueListEditorParser: TValueListEditor;
+    procedure ButtonOkClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  FormParserSet: TFormParserSet;
+
+implementation
+
+{$R *.lfm}
+
+{ TFormParserSet }
+
+procedure TFormParserSet.ButtonOkClick(Sender: TObject);
+begin
+  ValueListEditorParser.Values['LogEleTag']:=UpperCase(ValueListEditorParser.Values['LogEleTag']);
+  ValueListEditorParser.Values['LogEleAlertAttr']:=UpperCase(ValueListEditorParser.Values['LogEleAlertAttr']);
+end;
+
+end.
+
